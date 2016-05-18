3968b0f06e1cb700cf9699550d8be602faf2b895
imorenodev@gmail.com <imorenodev@gmail.com>
2016-05-18 19:17:23 -0400

    added next button to results component and tests

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -15,14 +15,24 @@ export default React.createClass({
   render: function() {
     return (
       <div className='results'>
-        {this.getPair().map((entry) => (
-          <div key={entry} className='entry'>
-            <h1>{entry}</h1>
-            <div className='voteCount'>
-              {this.getVotes(entry)}
+        <div className='tally'>
+          {this.getPair().map((entry) => (
+            <div key={entry} className='entry'>
+              <h1>{entry}</h1>
+              <div className='voteCount'>
+                {this.getVotes(entry)}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className='management'>
+          <button 
+            onClick={this.props.next}
+            className='next'
+            ref='next'>
+            {'Next'}
+          </button>
+        </div>
       </div>
     );
   }
--- test/components/Results_spec.jsx
+++ test/components/Results_spec.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   renderIntoDocument,
-  scryRenderedDOMComponentsWithClass
+  scryRenderedDOMComponentsWithClass,
+  Simulate
 } from 'react-addons-test-utils';
-import {List, Map} from 'immutable';
+import { List, Map } from 'immutable';
 import Results from '../../src/components/Results';
-import {expect} from 'chai';
+import { expect } from 'chai';
 
 describe('Results', () => {
 
@@ -23,6 +25,21 @@ describe('Results', () => {
     expect(train).to.contain('5');
     expect(days).to.contain('28 Days Later');
     expect(days).to.contain('0');
+  });
+
+  it('invokes the next callback when next button is clicked', () => {
+    let nextInvoked = false;
+    const next = () => nextInvoked = true;
+
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const component = renderIntoDocument(
+      <Results pair={pair}
+               tally={Map()}
+               next={next}/>
+    );
+    Simulate.click(ReactDOM.findDOMNode(component.refs.next));
+
+    expect(nextInvoked).to.equal(true);
   });
 
 });
