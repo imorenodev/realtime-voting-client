da68990cd1822366756af8ce96ced45a68d2c051
imorenodev@gmail.com <imorenodev@gmail.com>
2016-05-18 12:28:06 -0400

    added hasVoted and Winner conditions to Voting component

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,9 @@ import './styles.css';
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting 
+    pair={pair} 
+    hasVoted={'Trainspotting'} 
+    winner='Trainspotting' />,
   document.getElementById('app')
 );
