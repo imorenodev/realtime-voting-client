7ddcab8f6e15c37fa5016a17681f86d9f9349389
imorenodev@gmail.com <imorenodev@gmail.com>
2016-05-18 21:06:24 -0400

    added reducer logic to handle voting and handling hasVoted

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,37 @@
-import { Map } from 'immutable';
+import { Map, List } from 'immutable';
 
 function setState(state, newState) {
   return state.merge(newState);
 }
 
+function vote(state, entry) {
+  const currentPair = state.getIn(['vote', 'pair']);
+  if (currentPair && currentPair.includes(entry)) {
+    return state.set('hasVoted', entry);
+  } 
+  return state;
+}
+
+function resetVote(state) {
+  const hasVoted = state.get('hasVoted');
+  const currentPair = state.getIn(['vote', 'pair'], List());
+  // if state contains a hasVoted property BUT 
+  // the current pair doesn't include the movie title the client
+  // had voted on previously, then it's a new vote and 
+  // remove hasVoted property from state
+  if (hasVoted && !currentPair.includes(hasVoted)) {
+    return state.remove('hasVoted');
+  }
+  return state;
+}
+
 export default function(state = Map(), action) {
   switch (action.type) {
     case 'SET_STATE' :
-      return setState(state, action.state);
+      // composing functions, set the state and then reset vote if client already voted
+      return resetVote(setState(state, action.state));
+    case 'VOTE' :
+      return vote(state, action.entry);
     default :
       return state;
   }
